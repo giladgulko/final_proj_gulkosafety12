a1f31fe261fadfc8aee1257e2a36cb0beff72ee0
rupi602021 <rupi60.2021@gmail.com>
2021-03-28 13:32:29 +0300

    styles

--- final_proj_gulkosafety/Models/alert.cs
+++ final_proj_gulkosafety/Models/alert.cs
@@ -10,18 +10,18 @@ namespace final_proj_gulkosafety.Models
         int alert_num;
         string content;
         DateTime date;
-        DateTime time;
         int alert_type_num;
         string user_email;
+        int status;
 
-        public alert(int alert_num, string content,DateTime date,DateTime time,int alert_type_num, string user_email)
+        public alert(int alert_num, string content,DateTime date,int alert_type_num, string user_email,int status)
         {
             Alert_num = alert_num;
             Content = content;
             Date = date;
-            Time = time;
             Alert_type_num = alert_type_num;
             User_email = user_email;
+            Status = status;
         }
 
         public alert() { }
@@ -29,8 +29,9 @@ namespace final_proj_gulkosafety.Models
         public int Alert_num { get => alert_num; set => alert_num = value; }
         public string Content { get => content; set => content = value; }
         public DateTime Date { get => date; set => date = value; }
-        public DateTime Time { get => time; set => time = value; }
         public int Alert_type_num { get => alert_type_num; set => alert_type_num = value; }
         public string User_email { get => user_email; set => user_email = value; }
+        public int Status { get => status; set => status = value; }
+
     }
 }
