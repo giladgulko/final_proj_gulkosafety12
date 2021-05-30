1b25e2c8213d0de20c9ac52e3b3fd7e8250a2c66
rupi602021 <rupi60.2021@gmail.com>
2021-05-30 10:15:57 +0300

    Update SettingsController.cs

--- final_proj_gulkosafety/Controllers/SettingsController.cs
+++ final_proj_gulkosafety/Controllers/SettingsController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace final_proj_gulkosafety.Controllers
 {
-    public class SettingsController : ApiController
+    public class settingsController : ApiController
     {
         // GET api/<controller>
         public IEnumerable<string> Get()
